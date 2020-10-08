3548ae9e5ac6a15bd1639f6b6a227fcf724dfc35
Piotr Płoszczyca <piotrus.plo@gmail.com>
2020-10-08 11:56:01 +0200

    Do punktu 11

--- lab1/src/agh/cs/lab1/World.java
+++ lab1/src/agh/cs/lab1/World.java
@@ -1,2 +1,13 @@
-package agh.cs.lab1;public class World {
+package agh.cs.lab1;
+
+public class World {
+    public static void run(){
+        System.out.println("Zwierzak idzie do przodu.");
+    }
+
+    public static void main(String[] args){
+        System.out.println("System Start");
+        run();
+        System.out.println("System End");
+    }
 }
