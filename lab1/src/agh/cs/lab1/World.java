1083e445482b7e4d4fb498ba65350a284633cf5b
Piotr Płoszczyca <piotrus.plo@gmail.com>
2020-10-08 12:12:12 +0200

    Do punktu 15

--- lab1/src/agh/cs/lab1/World.java
+++ lab1/src/agh/cs/lab1/World.java
@@ -1,13 +1,20 @@
 package agh.cs.lab1;
 
 public class World {
-    public static void run(){
-        System.out.println("Zwierzak idzie do przodu.");
+    public static void run(String[] arr){
+//        System.out.println("Zwierzak idzie do przodu.");
+        System.out.print(arr[0]);
+        for(int i = 1; i < arr.length; i++){
+            System.out.print(",");
+            System.out.print(arr[i]);
+        }
+        System.out.println();
     }
 
     public static void main(String[] args){
+        String[] tab = {"Raz", "Dwa", "Trzy", "Dwa"};
         System.out.println("System Start");
-        run();
+        run(tab);
         System.out.println("System End");
     }
 }
